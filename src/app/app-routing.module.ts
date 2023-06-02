b7dc94ffaa2d04d88af3b4a8a24be75ca528bf1a
Victoria <veerod0804@yahoo.com>
2023-06-02 07:40:17 -0500

    adjusted wording in h2 elements on pages. Replaced dummy text.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ContactMeComponent } from './pages/contact-me/contact-me.component';
 import { DrawingsComponent } from './pages/drawings/drawings.component';
 import { GlassComponent } from './pages/glass/glass.component';
